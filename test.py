f53555b6692547302db8d212d6aec0779df7c716
Mike Choko <choko@chaksoft.fr>
2014-09-15 10:42:07 +0200

    TTF and UI components fix

--- test.py
+++ test.py
@@ -5,8 +5,14 @@ from state import *
 from ui import *
 
 class TestState(BaseState):
+	def __init__(self, engine):
+		BaseState.__init__(self, engine)
+		self.lbl1 = Label(self, (10, 10), 'Sansation_Light.ttf', (300, 30), caption="Test 1", charsize=42, bold=True, color=(255, 255, 255))
+		self.txt1 = TextBox(self, (40, 40), 'Sansation_Light.ttf', (300, 40), charsize=16)
+
 	def draw(self, window):
-		pass
+		self.lbl1.draw(window)
+		self.txt1.draw(window)
 
 	def handle_event(self, event):
 		pass
--- ui.py
+++ ui.py
@@ -11,11 +11,12 @@ import util
 
 	Each component can throw events
 """
-class Component(metaclass=ABCMeta):
+class Component(util.EventThrower, metaclass=ABCMeta):
 	def __init__(self, parent, position, font, **kwargs):
+		util.EventThrower.__init__(self)
 		self.position = position
 		self.font = font
-		self.parent
+		self.parent = parent
 		for k in kwargs:
 			setattr(self, k, kwargs[k])
 
@@ -77,7 +78,7 @@ class Label(SizedComponent):
 		self.color = color
 
 	def draw(self, window):
-		_inner = sf.Text(self.caption, self.font, self.charsize)
+		_inner = sf.Text(self.caption, sf.Font.from_file(self.font), self.charsize)
 		_inner.position = self.position
 		_inner.style = sf.Text.REGULAR
 		if self.bold:
@@ -86,18 +87,42 @@ class Label(SizedComponent):
 			_inner.style = _inner.style | sf.Text.ITALIC
 		if self.underlined:
 			_inner.style = _inner.style | sf.Text.UNDERLINED
-		_inner.color = sf.Color(self.color)
+		_inner.color = sf.Color(self.color[0], self.color[1], self.color[2])
 		
 		window.draw(_inner)
 		self.trigger('drawn', target=window)
 
 class TextBox(FocusedComponent):
-	def __init__(self, parent, position, font, size, initial='', placeholder='', **kwargs):
-		SizedComponent.__init__(self, parent, position, font, size, **kwargs)
+	def __init__(self, parent, position, font, size, initial='', placeholder='', charsize=14, **kwargs):
+		FocusedComponent.__init__(self, parent, position, font, size, **kwargs)
 		self.text = initial
 		self.placeholder = placeholder
+		self.charsize = charsize
+
+	def append_text(self, text):
+		self.text += text
+
+	def backspace(self):
+		if len(self.text) > 0:
+			self.text = self.text[:-1]
 
 	def draw(self, window):
-		pass
+		_rect = sf.RectangleShape(self.size)
+		_rect.position = self.position
+		_rect.outline_thickness = 2
+		_rect.outline_color = sf.Color(255, 255, 255)
+		_rect.fill_color = sf.Color(0, 0, 0)
+		if self.focused:
+			_rect.outline_color = sf.Color(self.bordercolor[0], self.bordercolor[1], self.bordercolor[2]) if hasattr(self, 'bordercolor') else sf.Color(60, 140, 200)
 
-Component.register(util.EventThrower)
+		_text = sf.Text(self.text + ('_' if self.focused else ''), sf.Font.from_file(self.font), self.charsize)
+		_text.position = (self.position[0] + 1, self.position[1] + 1)
+		if hasattr(self, 'padding'):
+			_text.position = (self.position[0] + 1 + self.padding, self.position[1] + 1)
+		if hasattr(self, 'color'):
+			_text.color = sf.Color(self.color[0], self.color[1], self.color[2])
+		else:
+			_text.color = sf.Color(0, 0, 0)
+
+		window.draw(_rect)
+		window.draw(_text)
